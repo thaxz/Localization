8303c0368dbf6870b096fcdf1903f26a6661d1e5
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2025-06-10 11:10:14 -0300

    first commit

---
+++ Localization/LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationApp.swift
+//  Localization
+//
+//  Created by thaxz on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
